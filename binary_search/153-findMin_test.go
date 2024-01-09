f7f4f785f76c7e7af81c4f817ac506ba3215295f
jiwenyuan <18233108619@163.com>
2024-01-09 11:36:49 +0800

    two points

---
+++ binary_search/153-findMin_test.go
@@ -0,0 +1,37 @@
+package binary_search
+
+import "testing"
+
+var findMinSources = []struct {
+	name string
+	nums []int
+	hope int
+}{
+	{
+		name: "test0",
+		nums: []int{3, 4, 5, 1, 2},
+		hope: 1,
+	},
+	{
+		name: "test1",
+		nums: []int{4, 5, 6, 7, 0, 1, 2},
+		hope: 0,
+	},
+	{
+		name: "test2",
+		nums: []int{11, 13, 15, 17},
+		hope: 11,
+	},
+}
+
+func TestFindMin(t *testing.T) {
+
+	for _, source := range findMinSources {
+		r := findMin(source.nums)
+		if r != source.hope {
+			t.Errorf("【%s】exec err, hope %v, got %v\n", source.name, source.hope, r)
+		} else {
+			t.Logf("【%s】exec ok\n", source.name)
+		}
+	}
+}
--- binary_search/167-twoSum.go
+++ binary_search/167-twoSum.go
@@ -2,6 +2,7 @@ package binary_search
 
 /*
 167. 两数之和 II - 输入有序数组 ~ 二分查找
+
 给你一个下标从 1 开始的整数数组 numbers ，该数组已按 非递减顺序排列  ，请你从数组中找出满足相加之和等于目标数 target 的两个数。
 如果设这两个数分别是 numbers[index1] 和 numbers[index2] ，则 1 <= index1 < index2 <= numbers.length 。
 以长度为2的整数数组 [index1, index2] 的形式返回这两个整数的下标 index1 和 index2。
